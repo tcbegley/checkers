bc64c3d1f43b32c698f2b834aa5a6f31a2fcf14e
tcbegley <tomcbegley@gmail.com>
2020-09-11 23:42:27 +0100

    Add counters, simple click event

--- frontend/src/components/Board.svelte
+++ frontend/src/components/Board.svelte
@@ -1,13 +1,27 @@
 <script>
   import Counter from "./Counter.svelte";
 
-  let indices = [...Array(64).keys()];
   let w;
+  let rows = [...Array(8).keys()];
+  let cols = [...Array(8).keys()];
 
-  function isDark(i) {
-    let col = i % 8;
-    let row = (i - col) / 8;
-    return (row + col) % 2 == 0;
+  let counters = [];
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 8; j += 2) {
+      counters.push({ row: i, col: j + (i % 2), dark: true, active: false });
+      counters.push({
+        row: 7 - i,
+        col: 7 - j - (i % 2),
+        dark: false,
+        active: false,
+      });
+    }
+  }
+
+  function handleClick(c) {
+    c.active = true;
+    console.log(c);
   }
 </script>
 
@@ -18,16 +32,14 @@
     max-width: 640px;
     min-width: 80px;
     position: relative;
+    border: 1px solid #444444;
   }
-  .board {
-    position: absolute;
+  .row {
     top: 0;
     left: 0;
     width: 100%;
-    height: 100%;
     display: grid;
     grid-template-columns: repeat(8, 1fr);
-    border: 1px solid #444444;
   }
 
   .square {
@@ -40,10 +52,20 @@
 </style>
 
 <div class="board-container" style="height: {w}px">
-  <div class="board" bind:clientWidth="{w}">
-    {#each indices as idx}
-      <div class="square {isDark(idx) ? 'dark' : ''}"></div>
-    {/each}
-  </div>
-  <Counter x="{80}" y="{160}" w="{80}" />
+  {#each rows as r}
+    <div class="row" bind:clientWidth="{w}" style="height: {w / 8}px">
+      {#each cols as c}
+        <div class="square {(r + c) % 2 === 0 ? 'dark' : ''}"></div>
+      {/each}
+    </div>
+  {/each}
+  {#each counters as c}
+    <Counter
+      counter="{c}"
+      w="{w / 8}"
+      handleClick="{() => {
+        c.active = !c.active;
+      }}"
+    />
+  {/each}
 </div>
--- frontend/src/components/Counter.svelte
+++ frontend/src/components/Counter.svelte
@@ -1,5 +1,7 @@
 <script>
-  export let x, y, w;
+  import classNames from "classnames";
+
+  export let counter, w, handleClick;
 </script>
 
 <style>
@@ -9,7 +11,7 @@
 
   .outer {
     position: absolute;
-    background-color: #eeaa55;
+    background-color: #cccccc;
     border-radius: 50%;
     width: 60%;
     height: 60%;
@@ -17,22 +19,37 @@
     margin-top: 20%;
   }
 
+  .outer.active {
+    box-shadow: 0px 0px 14px 3px rgba(193, 42, 47, 0.75);
+  }
+
+  .outer.dark {
+    background-color: #222222;
+  }
+
   .inner {
     position: absolute;
-    background-color: #eebb55;
+    background-color: #dddddd;
     border-radius: 50%;
-    width: 50%;
-    height: 50%;
-    margin-top: 25%;
-    margin-left: 25%;
+    width: 80%;
+    height: 80%;
+    margin-top: 10%;
+    margin-left: 10%;
+  }
+
+  .dark .inner {
+    background-color: #2a2a2a;
   }
 </style>
 
 <div
   class="container"
-  style="top: {y}px; left: {x}px; width: {w}px; height: {w}px;"
+  style="top: {counter.row * w}px; left: {counter.col * w}px; width: {w}px; height: {w}px;"
+  on:click="{handleClick}"
 >
-  <div class="outer">
+  <div
+    class="{classNames('outer', counter.dark && 'dark', counter.active && 'active')}"
+  >
     <div class="inner"></div>
   </div>
 </div>
